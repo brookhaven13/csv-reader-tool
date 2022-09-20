8f1e0bb8610f9b5c49972f3470961e588eee9319
Harley <yu.h.lin1991@gmail.com>
2022-09-21 03:12:35 +0800

    finish main page css style and data binding

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -6,38 +6,18 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./main.component.css'],
 })
 export class MainComponent implements OnInit {
-  @Input() csvData: any;
+  csvData: any;
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  loadCsvData(csvData: any) {
-    this.parseRawData(csvData);
+  loadCsvData(data: Array<string>) {
+    // get csv file data from upload-button component emitter
+    this.csvData = data;
   }
 
-  parseRawData(csvData: Array<any>) {
-    let splitLine = [];
-    let rawData = [];
-    let datasets: any = {
-      data: []
-    };
-
-    for(let i = 6; i < csvData.length; i++) {
-      splitLine = csvData[i].split(',');
-      let x = parseFloat(splitLine[0]);
-      let y = parseFloat(splitLine[1]);
-      if (csvData[i] === ',,') {
-        rawData.push(datasets);
-        datasets = {
-          data: []
-        }
-      } else if (isNaN(x) || isNaN(y)) {
-        // do nothing
-        // console.log(x, y);
-      } else {
-        datasets.data.push({x, y});
-      }
-    }
+  parseRawData(data: Array<string>) {
+    // Todo
   }
 }
